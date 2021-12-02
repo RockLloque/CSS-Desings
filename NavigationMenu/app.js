91bb1ea349ca12f1b577eb6e70ab06a0e8d28be4
rocklloque <thmschmahl@gmail.com>
2021-12-02 18:01:40 +0100

    added Navigation Menu, CSS and JS

---
+++ NavigationMenu/app.js
@@ -0,0 +1,12 @@
+const listItems = document.querySelectorAll(".list");
+
+function activeLink() {
+  listItems.forEach((item) => {
+    item.classList.remove("active");
+    this.classList.add("active");
+  });
+}
+
+listItems.forEach((item) => {
+  item.addEventListener("click", activeLink);
+});
